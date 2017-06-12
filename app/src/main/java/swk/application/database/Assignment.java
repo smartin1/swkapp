a9caf46d197df4bdbc5f783dfc13787b28f53afd
endres <martin.endres@live.de>
2017-06-12 07:34:50 +0200

    Assignment Update

--- app/src/main/java/swk/application/database/Assignment.java
+++ app/src/main/java/swk/application/database/Assignment.java
@@ -5,4 +5,90 @@ package swk.application.database;
  */
 
 public class Assignment {
+    int id;
+    String assignmentid;
+    String cwmp;
+    String gid;
+    String cid;
+    String wid;
+    String valid_from;
+    String valid_until;
+
+
+    public Assignment(String assignmentid, String cwmp ,String gid, String cid,
+               String wid, String valid_from, String valid_until) {
+        this.assignmentid=assignmentid;
+        this.cwmp = cwmp;
+        this.gid = gid;
+        this.cid = cid;
+        this.wid = wid;
+        this.valid_from = valid_from;
+        this.valid_until= valid_until;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAssignmentid(){
+        return assignmentid;
+    }
+
+    public void setAssignmentid(String assignmentid){
+        this.assignmentid=assignmentid;
+    }
+
+    public String getCwmp() {
+        return cwmp;
+    }
+
+    public void setCwmp(String cwmp) {
+        this.cwmp = cwmp;
+    }
+
+    public String getGid() {
+        return gid;
+    }
+
+    public void setGid(String gid) {
+        this.gid = gid;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getWid() {
+        return wid;
+    }
+
+    public void setWid(String wid) {
+        this.wid = wid;
+    }
+
+    public String getValid_from() {
+        return valid_from;
+    }
+
+    public void setValid_from(String valid_from) {
+        this.valid_from = valid_from;
+    }
+
+    public String getValid_until() {
+        return valid_until;
+    }
+
+    public void setValid_until(String valid_until) {
+        this.valid_until = valid_until;
+    }
 }
